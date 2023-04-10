0c4511faf8433273336bc1058afb6ec41b409532
CanerCagri <canerrcagrii@gmail.com>
2023-04-10 21:21:29 +0300

    Initial Commit

---
+++ SwiftUI-Bar-Chart/SwiftUI_Bar_ChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Bar_ChartApp.swift
+//  SwiftUI-Bar-Chart
+//
+//  Created by Caner Çağrı on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Bar_ChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
